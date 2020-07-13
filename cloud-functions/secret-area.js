aeb4182ebf81f3cbe8b2e8a6407add0f92698531
Martin <m.larrabu@gmail.com>
2020-07-12 22:09:28 -0500

    created first cloud function

---
+++ cloud-functions/secret-area.js
@@ -0,0 +1,8 @@
+const { useCallback } = require("react")
+
+exports.handler = function(event, context, callback){
+    callback(null, {
+        statusCode: 200,
+        body: "Welcome to the super secret area"
+    })
+}
