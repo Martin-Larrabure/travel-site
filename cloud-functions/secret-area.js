f9409d75ebb8d12cfd93e7c51dc4b0870b917f3c
Martin <m.larrabu@gmail.com>
2020-07-13 11:16:06 -0500

    updated cloud function

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,8 +1,31 @@
 const { useCallback } = require("react")
 
 exports.handler = function(event, context, callback){
-    callback(null, {
-        statusCode: 200,
-        body: "Welcome to the secret area"
-    })
+    const sevcretContent = `
+    <h3>Welcome to the secret area</h3>
+    <p>Here the sky is <strong>blue</strong>, and the grass is green</p>
+    
+    `
+
+    
+    let body
+
+    if(event.body){
+        body=JSON.parse(event.body)
+    }else{
+        body = {}
+    }
+
+    if (body.password == "javascript"){
+        callback(null, {
+            statusCode: 200,
+            body: sevcretContent
+        })
+    }else{
+        callback(null, {
+            statusCode: 401,
+        })
+    }
+
+   
 }
