c6cdd8c364212034888f3a96efad5e6e0e24cc87
Martin <m.larrabu@gmail.com>
2020-07-24 09:41:32 -0500

    built

---
+++ app/assets/scripts/modules/MyAmazingComponent.js
@@ -0,0 +1,10 @@
+import React from 'react'
+function MyAmazingComponent(){
+    return(
+        <div>
+            <h1 className="section-title section-title__blue">This is my amazing react component</h1>
+            <p>React is cool</p>
+        </div>
+    )
+}
+export default MyAmazingComponent
